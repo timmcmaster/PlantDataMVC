a60dace2671487b57627053bc16ae498df0a3bfb
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2017-06-05 18:50:10 +1000

    Added marker popup and cursor change (needs bootstrap to work)

--- PlantDataMVC.UI/Scripts/OpenMapsLibrary.js
+++ PlantDataMVC.UI/Scripts/OpenMapsLibrary.js
@@ -1,21 +1,25 @@
 ﻿// using module pattern to restrict function clashes
 var Mapping = (function () {
 
-    var addMarker = function (map, latitude, longitude) {
+    // addMarker
+    var addMarker = function (imageSrc, map, latitude, longitude) {
         var iconFeature = new ol.Feature({
             geometry: new ol.geom.Point(ol.proj.fromLonLat([longitude, latitude])),
             name: 'My Location'
         });
 
+        // define style and image
         var iconStyle = new ol.style.Style({
             image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
-                anchor: [0.5, 46],
+                anchor: [0.5, 1.0],
                 anchorXUnits: 'fraction',
-                anchorYUnits: 'pixels',
-                src: 'https://openlayers.org/en/v4.1.1/examples/data/icon.png'
+                anchorYUnits: 'fraction',
+                scale: 0.1,
+                src: imageSrc
             }))
         });
 
+        // Set style for feature
         iconFeature.setStyle(iconStyle);
 
         var vectorSource = new ol.source.Vector({
@@ -28,8 +32,53 @@ var Mapping = (function () {
 
         // initial case we can addLayer, but really should try and get it first
         map.addLayer(vectorLayer);
+
+        // Set up popup for icon
+        var element = document.getElementById('popup');
+
+        var popup = new ol.Overlay({
+            element: element,
+            positioning: 'bottom-center',
+            stopEvent: false,
+            offset: [0, -50]
+        });
+        map.addOverlay(popup);
+
+        // TODO: popover is part of bootstrap library which is not yet included (hence errors)
+        // change mouse cursor when over marker
+        map.on('pointermove', function (e) {
+            if (e.dragging) {
+                $(element).popover('destroy');
+                return;
+            }
+            var pixel = map.getEventPixel(e.originalEvent);
+            var hit = map.hasFeatureAtPixel(pixel);
+            map.getTarget().style.cursor = hit ? 'pointer' : '';
+        });
+
+        // display popup on click
+        map.on('click', function (evt) {
+            var feature = map.forEachFeatureAtPixel(evt.pixel,
+                function (feature) {
+                    return feature;
+                });
+            if (feature) {
+                var coordinates = feature.getGeometry().getCoordinates();
+                popup.setPosition(coordinates);
+                $(element).popover({
+                    'placement': 'top',
+                    'html': true,
+                    'content': feature.get('name')
+                });
+                $(element).popover('show');
+            } else {
+                $(element).popover('destroy');
+            }
+        });
+
     }
 
+    // createMap
     var createMap = function (id, latitude, longitude, zoomLevel) {
         var map = new ol.Map({
             target: id,
