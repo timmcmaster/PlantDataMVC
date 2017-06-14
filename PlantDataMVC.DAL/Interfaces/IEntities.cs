e83a042124b3e000a928206d475a1e5e3a6a2baf
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2017-06-14 16:53:21 +1000

    Split out IEntity to it's own file

--- PlantDataMVC.DAL/Interfaces/IEntities.cs
+++ PlantDataMVC.DAL/Interfaces/IEntities.cs
@@ -5,17 +5,6 @@ using System.Text;
 
 namespace PlantDataMVC.DAL.Interfaces
 {
-    /// <summary>
-    /// This interface is the basic interface for entity objects passed outside the DAL.
-    /// Interactions between the Data Access Layer and the Business Layer 
-    /// are all done with IEntity-derived objects. 
-    /// Each object type should have an IEntity derived class to contain type-specific properties
-    /// </summary>
-    public interface IEntity
-    {
-        int Id { get; }
-    }
-
     public interface IGenus: IEntity
     {
         string LatinName { get; }
