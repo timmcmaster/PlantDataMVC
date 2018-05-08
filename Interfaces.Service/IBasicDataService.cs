ad308b5964ee4654c2a82bf7fb932d9d458a9ae5
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2018-05-08 18:02:44 +1000

    Moved service contract stuff.

--- Interfaces.Service/IBasicDataService.cs
+++ Interfaces.Service/IBasicDataService.cs
@@ -1,13 +1,26 @@
 ﻿using Interfaces.Domain;
+using System.ServiceModel;
 
 namespace Interfaces.Service
 {
+    // TODO: Sort out whether I can put WCF-specific attriubutes (contract stuff) elsewhere 
+    // where we need WCF stuff (and not before)
+    [ServiceContract]
     public interface IBasicDataService<T> where T : IDomainEntity
     {
+        [OperationContract]
         ICreateResponse<T> Create(ICreateRequest<T> request);
+
+        [OperationContract]
         IViewResponse<T> View(IViewRequest<T> request);
+
+        [OperationContract]
         IUpdateResponse<T> Update(IUpdateRequest<T> request);
+
+        [OperationContract]
         IDeleteResponse<T> Delete(IDeleteRequest<T> request);
+
+        [OperationContract]
         IListResponse<T> List(IListRequest<T> request);
     }
 }
