d13acd8fe3622585f782c7dfdb66320c8e4eb49b
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2018-08-21 18:26:40 +1000

    Fixed rounding on Site lat/long (DB definition was wrong) Added DB scripts - need to find right way to incude DB in project

---
+++ PlantDataMVC.Tests.DAL/IntegrationTests/SiteRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using Framework.DAL.EF;
+using Interfaces.DAL.DataContext;
+using Interfaces.DAL.UnitOfWork;
+using PlantDataMVC.Entities.Context;
+using PlantDataMVC.Entities.Models;
+using System.Data.Entity.Validation;
+using Xunit;
+
+namespace PlantDataMVC.Tests.DAL.IntegrationTests
+{
+    public class SiteRepositoryTest
+    {
+        [Fact]
+        public void CanUpdateSiteAndGetBackSame()
+        {
+
+            using (IDataContextAsync plantDataDBContext = new PlantDataDbContext())
+            using (IUnitOfWorkAsync uow = new UnitOfWork(plantDataDBContext))
+            {
+                // Arrange
+                var id = 6;
+                var repository = uow.Repository<Site>();
+                var site = repository.GetItemById(id);
+
+                // Act
+                site.Latitude = 1.5m;
+                site.Longitude = 1.5m;
+
+                try { 
+                    var updatedSite = repository.Save(site);
+                    uow.SaveChanges();
+                }
+                catch (DbEntityValidationException ex)
+                {
+
+                }
+
+                var retrievedSite = repository.GetItemById(id);
+
+                // Assert
+                Assert.NotNull(retrievedSite);
+                Assert.Equal(site.Latitude, retrievedSite.Latitude);
+                Assert.Equal(site.Longitude, retrievedSite.Longitude);
+            }
+        }
+    }
+}
