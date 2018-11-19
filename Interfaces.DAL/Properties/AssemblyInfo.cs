bb58542400c70b56ec2ff962c5de1942b7318e49
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2018-11-19 19:34:13 +1000

    General cleanup, after trying to migrate to new project format

--- Interfaces.DAL/Properties/AssemblyInfo.cs
+++ Interfaces.DAL/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Framework.DAL")]
+[assembly: AssemblyTitle("Interfaces.DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Framework.DAL")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Interfaces.DAL")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("39a4295b-11f9-4b3e-8527-d99fff87e10b")]
+[assembly: Guid("96fd9436-de5c-432a-94cb-ce5e63ee2405")]
 
 // Version information for an assembly consists of the following four values:
 //
