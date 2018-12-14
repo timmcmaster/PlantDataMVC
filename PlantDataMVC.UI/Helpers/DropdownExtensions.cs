0bea69fda65a468b5991b13d0c659faebea3a675
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2018-12-14 16:29:43 +1000

    Added hacky fix to map IDomainEntity to equivalent IDataServiceBase<IDomainEntity> via WCF interface type (e.g. IPlantDataService)

--- PlantDataMVC.UI/Helpers/DropdownExtensions.cs
+++ PlantDataMVC.UI/Helpers/DropdownExtensions.cs
@@ -1,7 +1,10 @@
 ﻿using Framework.Service.Entities;
 using Interfaces.Domain;
 using Interfaces.Service;
+using PlantDataMVC.Domain.Entities;
+using PlantDataMVC.Service.ServiceContracts;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
@@ -14,7 +17,7 @@ namespace PlantDataMVC.UI.Helpers
     /// </summary>
     public static class DropDownExtensions
     {
-        public static MvcHtmlString DropDownFor<TModel,TItem>(this HtmlHelper<TModel> htmlHelper,
+        public static MvcHtmlString DropDownFor<TModel, TItem>(this HtmlHelper<TModel> htmlHelper,
             Func<string> fieldName,
             Expression<Func<TModel, TItem>> expression,     // Selects the referenced entity from the model
             Func<TItem, string> displayValueSelector,       // Selects the display field from the entity
@@ -28,9 +31,18 @@ namespace PlantDataMVC.UI.Helpers
             var selectedItem = (TItem)metadata.Model;
 
             // Get list of options from dataService
-            // TODO: need to fix this, as IDataServiceBase instances aren't currently registered in IoC
-            var dataService = DependencyResolver.Current.GetService<IDataServiceBase<TItem>>();
-            var items = dataService.List(new ListRequest<TItem>()).Items;
+
+            // TODO: this doesn't work as IDataServiceBase instances aren't currently registered in IoC
+            //var dsTest = DependencyResolver.Current.GetService<IDataServiceBase<TItem>>();
+
+            // This now works after implementing ServiceContract and OperationContract on IDataServiceBase<T> 
+            Type interfaceType = GetDataServiceInterfaceFor<TItem>();
+            var dataService = DependencyResolver.Current.GetService(interfaceType) as IDataServiceBase<TItem>;
+            IList<TItem> items = new List<TItem>();
+            if (dataService != null)
+            {
+                items = dataService.List(new ListRequest<TItem>()).Items;
+            }
 
             var selectListItems = items.Select(x => new SelectListItem
             {
@@ -39,7 +51,34 @@ namespace PlantDataMVC.UI.Helpers
                 Selected = (dataValueSelector(x).Equals(dataValueSelector(selectedItem)))
             });
 
-            return htmlHelper.DropDownList(fieldName(), selectListItems,"Select an option");
+            return htmlHelper.DropDownList(fieldName(), selectListItems, "Select an option");
+        }
+
+        // HACK: Very very hacky quick hack to return service interface for given IEntity type
+        public static Type GetDataServiceInterfaceFor<TItem>() where TItem : IDomainEntity
+        {
+            if (typeof(TItem) == typeof(Plant))
+                return typeof(IPlantDataService);
+            //else if (typeof(TItem) == typeof(PlantPriceListType))
+            //    return typeof();
+            //else if (typeof(TItem) == typeof(PlantProductPrice))
+            //    return typeof();
+            else if (typeof(TItem) == typeof(PlantProductType))
+                return typeof(IPlantProductTypeDataService);
+            else if (typeof(TItem) == typeof(PlantSeed))
+                return typeof(IPlantSeedDataService);
+            else if (typeof(TItem) == typeof(PlantSeedSite))
+                return typeof(IPlantSeedSiteDataService);
+            else if (typeof(TItem) == typeof(PlantSeedTray))
+                return typeof(IPlantSeedTrayDataService);
+            else if (typeof(TItem) == typeof(PlantStockEntry))
+                return typeof(IPlantStockEntryDataService);
+            else if (typeof(TItem) == typeof(PlantStockTransaction))
+                return typeof(IPlantStockTransactionDataService);
+            else if (typeof(TItem) == typeof(PlantStockTransactionType))
+                return typeof(IPlantStockTransactionTypeDataService);
+            else
+                throw new Exception("corresponding service not found");
         }
     }
 }
