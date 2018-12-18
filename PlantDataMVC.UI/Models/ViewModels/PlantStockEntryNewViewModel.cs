0197d81e51ee99b8961d9e1e39c61e07ee1ad7ab
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2018-12-18 13:54:19 +1000

    Remove commemnted code

--- PlantDataMVC.UI/Models/ViewModels/PlantStockEntryNewViewModel.cs
+++ PlantDataMVC.UI/Models/ViewModels/PlantStockEntryNewViewModel.cs
@@ -5,15 +5,8 @@ namespace PlantDataMVC.UI.Models
 {
     public class PlantStockEntryNewViewModel
     {
-        // TODO: Use dropdown for selection
         [Display(Name = "Species Name")]
         public Plant PlantSpecies { get; set; }
-
-        //[Display(Name = "Species Id")]
-        //public int SpeciesId { get; set; }
-
-        //[Display(Name = "Species Name")]
-        //public string SpeciesBinomial { get; private set; }
 
         [Display(Name = "Product Type")]
         public PlantProductType ProductType { get; set; }
