a858fa06604199803687c2b3b6a18714fe90e40c
Timmo <mcmaster.tim@gmail.com>
2019-01-04 00:11:40 +1000

    Tweaked and commented IDataServiceBase

--- Interfaces.Service/IDataServiceBase.cs
+++ Interfaces.Service/IDataServiceBase.cs
@@ -7,19 +7,24 @@ namespace Interfaces.Service
     [ServiceContract]
     public interface IDataServiceBase<T> where T : IDomainEntity
     {
+        // POST: api/Plant
         [OperationContract]
-        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped,Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "")]
         ICreateResponse<T> Create(ICreateRequest<T> request);
 
+        // GET: api/Plant/5
         [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "api/Plant/{id}")]
         IViewResponse<T> View(IViewRequest<T> request);
 
+        // PUT: api/Plant/5
         [OperationContract]
         IUpdateResponse<T> Update(IUpdateRequest<T> request);
 
+        // DELETE: api/Plant/5
         [OperationContract]
         IDeleteResponse<T> Delete(IDeleteRequest<T> request);
 
+        // GET: api/Plant
         [OperationContract]
         IListResponse<T> List(IListRequest<T> request);
     }
