d80a7539acfeed2941158e1c4348f5307c9d7b07
Timmo <mcmaster.tim@gmail.com>
2019-01-06 00:53:23 +1000

    Got some webHttp processes working (view, list, delete). Still working on create and update

--- Interfaces.Service/IDataServiceBase.cs
+++ Interfaces.Service/IDataServiceBase.cs
@@ -9,22 +9,27 @@ namespace Interfaces.Service
     {
         // POST: api/Plant
         [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "Item")]
         ICreateResponse<T> Create(T item);
 
         // GET: api/Plant/5
         [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "Item?id={id}")]
         IViewResponse<T> View(int id);
 
         // PUT: api/Plant/5
         [OperationContract]
+        //[WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "Item?id={id}")]
         IUpdateResponse<T> Update(T item);
 
         // DELETE: api/Plant/5
         [OperationContract]
+        [WebInvoke(Method = "DELETE", ResponseFormat = WebMessageFormat.Json, UriTemplate = "Item?id={id}")]
         IDeleteResponse<T> Delete(int id);
 
         // GET: api/Plant
         [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "Item")]
         IListResponse<T> List();
     }
 }
