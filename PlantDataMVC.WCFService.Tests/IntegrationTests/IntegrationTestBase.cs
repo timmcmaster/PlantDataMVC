97b2c46188389169c358e2f9a3aa9adf24163d27
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2019-01-18 19:10:29 +1000

    Got tests working after reinstall of many NuGet pkgs

---
+++ PlantDataMVC.WCFService.Tests/IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Nito.AsyncEx;
+using Xunit;
+
+namespace PlantDataMVC.WCFService.Tests.IntegrationTests
+{
+    /// <summary>
+    ///   <P>Base class for integration tests in this namespace</P>
+    ///   <P>This class will clear the database using the Respawn package at beginning of every test.</P>
+    ///   Implements Xunit.IAsyncLifetime to manage cleaning up after async tasks.
+    /// </summary>
+    public abstract class IntegrationTestBase : IAsyncLifetime
+    {
+        //private static bool _initialized;
+        private static readonly AsyncLock _mutex = new AsyncLock();
+
+        ///// <summary>
+        ///// Version for reset only at creation of class
+        ///// Called immediately after the class has been created, before it is used.
+        ///// </summary>
+        ///// <returns></returns>
+        //public virtual async Task InitializeAsync()
+        //{
+        //    if (_initialized)
+        //        return;
+
+        //    using (await _mutex.LockAsync())
+        //    {
+        //        if (_initialized)
+        //            return;
+
+        //        await DBFixture.ResetCheckpoint();
+
+        //        _initialized = true;
+        //    }
+        //}
+
+        // Version for reset every test
+        public virtual async Task InitializeAsync()
+        {
+            using (await _mutex.LockAsync())
+            {
+                await DbFixture.ResetCheckpoint();
+            }
+        }
+        /// <summary>
+        /// Called when an object is no longer needed. Called just before <see cref="M:System.IDisposable.Dispose" />
+        /// if the class also implements that.
+        /// </summary>
+        /// <returns></returns>
+        public virtual Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ PlantDataMVC.WCFService.Tests/UnitTests/DtoToDalMappingFacts.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using AutoMapper;
+using PlantDataMVC.DTO.Mappers;
+using Xunit;
+
+namespace PlantDataMVC.WCFService.Tests.UnitTests
+{
+    public class DtoToDalMappingFacts : IDisposable
+    {
+        public DtoToDalMappingFacts()
+        {
+            // Reset Mapper before configuring
+            Mapper.Reset();
+            // Configure the mapper at start of each test
+            AutoMapperCoreConfiguration.Configure();
+        }
+
+        public void Dispose()
+        {
+            // Reset Mapper at end of each test
+            //Mapper.Reset();
+        }
+
+        [Fact]
+        public void TestMappingConfiguration()
+        {
+            Mapper.AssertConfigurationIsValid();
+        }
+
+        //[Fact]
+        //public void TestPlantToGenusMapping()
+        //{
+        //    // Arrange
+        //    var plant = PlantBuilder.aPlant().withRandomValues().Build();
+        //    var expectedGenus = new Genus()
+        //    {
+        //        Id = plant.Id,
+        //        LatinName = plant.GenericName
+        //    };
+
+        //    // Act
+        //    Genus result = Mapper.Map<Plant, Genus>(plant);
+
+        //    // Assert
+        //    result.Should().BeEquivalentTo(expectedGenus, options => options
+        //                                                    .Including(g => g.LatinName));
+        //}
+
+        //[Fact]
+        //public void TestPlantToSpeciesMapping()
+        //{
+        //    // Arrange
+        //    var plant = PlantBuilder.aPlant().withRandomValues().Build();
+        //    var expectedSpecies = new Species()
+        //    {
+        //        Id = plant.Id,
+        //        SpecificName = plant.SpecificName,
+        //        CommonName = plant.CommonName,
+        //        Description = plant.Description,
+        //        //GenusLatinName = plant.GenusLatinName,
+        //        Native = plant.Native,
+        //        PropagationTime = plant.PropagationTime
+        //    };
+
+        //    // Act
+        //    Species result = Mapper.Map<Plant, Species>(plant);
+
+        //    // Assert
+        //    result.Should().BeEquivalentTo(expectedSpecies, options => options
+        //                                                    .Including(s => s.Id)
+        //                                                    .Including(s => s.SpecificName)
+        //                                                    .Including(s => s.CommonName)
+        //                                                    .Including(s => s.Description)
+        //                                                    .Including(s => s.Native)
+        //                                                    .Including(s => s.PropagationTime));
+        //}
+
+    }
+}
