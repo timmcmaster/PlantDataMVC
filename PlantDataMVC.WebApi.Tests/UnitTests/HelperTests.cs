5184500eaae9c00f5fc9c16bfb53acbdcbd9ee55
Timmo <mcmaster.tim@gmail.com>
2019-01-24 01:30:59 +1000

    Added WebApi Test project and worked on data shaping

---
+++ PlantDataMVC.WebApi.Tests/UnitTests/HelperTests.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces.DTO;
+using PlantDataMVC.DTO.Dtos;
+using PlantDataMVC.WebApi.Helpers;
+using Xunit;
+
+namespace PlantDataMVC.WebApi.Tests.UnitTests
+{
+    public class HelperTests
+    {
+        [Fact]
+        public void TestGetRelatedObjects()
+        {
+            var relatedObjs = DataShaping.GetRelatedDtoObjects(new SpeciesDto());
+        }
+
+    }
+}
--- PlantDataMVC.WebApi/Helpers/DataShaping.cs
+++ PlantDataMVC.WebApi/Helpers/DataShaping.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using Interfaces.DTO;
+using System.Collections;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Reflection;
-using Interfaces.DTO;
 
 namespace PlantDataMVC.WebApi.Helpers
 {
@@ -16,7 +18,7 @@ namespace PlantDataMVC.WebApi.Helpers
         /// <param name="fieldList">The field list.</param>
         /// <param name="childrenToInclude"></param>
         /// <returns></returns>
-        public static object CreateDataShapedObject<TDto>(TDto dto, List<string> fieldList, List<string> childrenToInclude) where TDto : IDto
+        public static object CreateDataShapedObject<TDto>(TDto dto, List<string> fieldList/*, List<string> childrenToInclude*/) where TDto : IDto
         {
             if (!fieldList.Any())
             {
@@ -24,23 +26,28 @@ namespace PlantDataMVC.WebApi.Helpers
             }
             else
             {
+                var relatedObjects = GetRelatedDtoObjects(dto);
+
+                var childrenToInclude =
+                    GetChildObjectsToInclude(relatedObjects.Select(p => p.Name).ToList(), fieldList);
+
                 // TODO: get DTO for 
                 // Strip out any child fields from list of fields
                 List<string> fieldsOfChildren = fieldList.Where(f => f.Contains('.')).ToList();
 
-                // Determine fields for each specific child object
-                foreach (string childName in childrenToInclude)
-                {
-                    List<string> thisChildFields = fieldsOfChildren.Where(f => f.ToLower().StartsWith(childName.ToLower())).ToList();
+                //// Determine fields for each specific child object
+                //foreach (string childName in childrenToInclude)
+                //{
+                //    List<string> thisChildFields = fieldsOfChildren.Where(f => f.ToLower().StartsWith(childName.ToLower())).ToList();
 
-                    // Strip childName and "." from list of fields
-                    thisChildFields = thisChildFields.Select(f => f.Substring(f.IndexOf(".") + 1)).ToList();
+                //    // Strip childName and "." from list of fields
+                //    thisChildFields = thisChildFields.Select(f => f.Substring(f.IndexOf(".") + 1)).ToList();
 
-                    // Get DTO for child object
+                //    // Get DTO for child object
 
-                    // Create data shaped object for child and add to corresponding collection for this object
-                    var childShapedObject = CreateDataShapedObject<TDto>()
-                }
+                //    // Create data shaped object for child and add to corresponding collection for this object
+                //    var childShapedObject = CreateDataShapedObject<TDto>()
+                //}
 
                 // create a new ExpandoObject & dynamically create the properties for this object
                 ExpandoObject objectToReturn = new ExpandoObject();
@@ -56,7 +63,7 @@ namespace PlantDataMVC.WebApi.Helpers
                     var fieldValue = fieldPropInfo.GetValue(dto, null);
 
                     // add the field to the ExpandoObject with the actual property name from the object
-                    ((IDictionary<string, object>) objectToReturn).Add(fieldPropInfo.Name, fieldValue);
+                    ((IDictionary<string, object>)objectToReturn).Add(fieldPropInfo.Name, fieldValue);
                 }
 
                 return objectToReturn;
@@ -64,7 +71,7 @@ namespace PlantDataMVC.WebApi.Helpers
         }
 
         /// <summary>
-        /// Gets the child objects to include.
+        /// Gets the child objects to include, given a list of all fields and a list of child objects
         /// </summary>
         /// <param name="childObjects">The child objects.</param>
         /// <param name="lstOfFields">The LST of fields.</param>
@@ -83,6 +90,41 @@ namespace PlantDataMVC.WebApi.Helpers
             }
 
             return childObjectsToInclude;
+        }
+
+        /// <summary>
+        /// Gets the child objects to include, given a list of all fields and a list of child objects
+        /// </summary>
+        /// <param name="childObjects">The child objects.</param>
+        /// <param name="lstOfFields">The LST of fields.</param>
+        /// <returns></returns>
+        public static List<PropertyInfo> GetRelatedDtoObjects<TDto>(TDto dto) where TDto : IDto
+        {
+            var relatedDtoObjects = new List<PropertyInfo>();
+
+            // Scan all properties and get those which implement IDto or ICollection of IDto
+            var fieldType = typeof(TDto);
+
+            foreach (var propInfo in fieldType.GetProperties())
+            {
+                var propertyInterfaces = propInfo.PropertyType.GetInterfaces();
+
+                // if property implements IDto
+                if (typeof(IDto).IsAssignableFrom(propInfo.PropertyType))
+                {
+                    relatedDtoObjects.Add(propInfo);
+                }
+
+                // if property is ICollection of type that implements IDto
+                bool IsDtoCollection(Type t) => t.IsGenericType && typeof(ICollection<>).IsAssignableFrom(t.GetGenericTypeDefinition()) && t.GetGenericArguments().Any(a => typeof(IDto).IsAssignableFrom(a));
+
+                if (IsDtoCollection(propInfo.PropertyType))
+                {
+                    relatedDtoObjects.Add(propInfo);
+                }
+           }
+
+            return relatedDtoObjects;
         }
     }
 }
