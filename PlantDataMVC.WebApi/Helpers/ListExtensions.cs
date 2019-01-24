6bc70140cb8720534f5048ca697ba166d20e04a1
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2019-01-24 17:12:04 +1000

    Added ListExtensions file

---
+++ PlantDataMVC.WebApi/Helpers/ListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PlantDataMVC.WebApi.Helpers
+{
+    public static class ListExtensions
+    {
+        public static void RemoveRange<T>(this List<T> source, IEnumerable<T> rangeToRemove)
+        {
+            if (rangeToRemove == null | !rangeToRemove.Any())
+                return;
+
+            foreach (T item in rangeToRemove)
+            {
+                source.Remove(item);
+            }
+
+
+        }
+    }
+}
