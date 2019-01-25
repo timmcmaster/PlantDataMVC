215bede1cccd2e7a0af7ff8f6ec29f85add9b0ff
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2019-01-25 18:56:17 +1000

    WIP: Working on OAuth/OpenID server

---
+++ PlantDataMVC.IdSrv/Config/Clients.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer3.Core.Models;
+
+namespace PlantDataMVC.IdSrv.Config
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    Enabled = true,
+                    ClientName = "PlantData MVC Client (Hybrid Flow)",
+                    ClientId = "mvc",
+                    Flow = Flows.Hybrid,
+                    RequireConsent = true
+                }
+            };
+        }
+    }
+}
