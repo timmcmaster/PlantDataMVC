729428cac563e8f34134bb4c31a5b91f29d93f1e
Timmo <mcmaster.tim@gmail.com>
2019-01-31 23:13:47 +1000

    Added JournalEntriesController to web api

---
+++ PlantDataMVC.WebApi/Controllers/JournalEntriesController.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CacheCow.Server.WebApi;
+using Interfaces.DAL.UnitOfWork;
+using Marvin.JsonPatch;
+using PlantDataMVC.DTO.Dtos;
+using PlantDataMVC.Entities.Models;
+using PlantDataMVC.Service;
+using PlantDataMVC.WebApi.Helpers;
+
+namespace PlantDataMVC.WebApi.Controllers
+{
+    [RoutePrefix("api")]
+    public class JournalEntriesController : ApiController
+    {
+        private const int MaxPageSize = 100;
+
+        private readonly IUnitOfWorkAsync _unitOfWorkAsync;
+        private readonly IJournalEntryService _service;
+
+        public JournalEntriesController(IUnitOfWorkAsync unitOfWorkAsync,
+            IJournalEntryService service)
+        {
+            _service = service;
+            _unitOfWorkAsync = unitOfWorkAsync;
+        }
+
+        // GET: api/Species
+        [HttpCache(DefaultExpirySeconds = 300)]
+        [HttpGet]
+        [Route("PlantStock/{plantStockId}/JournalEntries", Name = "EntriesForStock")]
+        public IHttpActionResult Get(
+            int plantStockId,
+            string sort = "id", 
+            int page = 1, int pageSize = MaxPageSize,
+            string fields = null)
+        {
+            try
+            {
+                // TODO: Current state doesn't return children by default, can only get with "fields" option
+                // need to determine expected behaviour
+
+                var childDtosToInclude = new List<string>();
+                var lstOfFields = new List<string>();
+
+                if (fields != null)
+                {
+                    lstOfFields = fields.Split(',').ToList();
+                    childDtosToInclude = DataShaping.GetIncludedObjectNames<JournalEntryDto>(lstOfFields);
+                }
+
+                if (pageSize > MaxPageSize)
+                {
+                    pageSize = MaxPageSize;
+                }
+
+                var context = _service.Queryable();
+
+                IQueryable<JournalEntryDto> dtos = context
+                    .ProjectTo<JournalEntryDto>(null, childDtosToInclude.ToArray())
+                    .ApplySort(sort)
+                    .Where(s => s.PlantStockId == plantStockId);
+
+                var paginationHeaders = PagingHelper.GetPaginationHeaders(
+                    Url,
+                    dtos,
+                    "EntriesForStock",
+                    new
+                    {
+                        sort = sort,
+                        plantStockId = plantStockId,
+                        fields = fields
+                    },
+                    page,
+                    pageSize);
+
+                HttpContext.Current.Response.Headers.Add(paginationHeaders);
+
+                var itemList = dtos
+                    .Paginate(page, pageSize)
+                    .ToList()
+                    .Select(dto => DataShaping.CreateDataShapedObject(dto, lstOfFields));
+                
+                return Ok(itemList);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError();
+            }
+        }
+
+        // GET: api/Plant/5
+        [HttpCache(DefaultExpirySeconds = 300)]
+        [HttpGet]
+        public IHttpActionResult Get(int id, string fields = null)
+        {
+            try
+            {
+                //var childDtosToInclude = new List<string>();
+                var lstOfFields = new List<string>();
+
+                if (fields != null)
+                {
+                    lstOfFields = fields.Split(',').ToList();
+                    //childDtosToInclude = DataShaping.GetIncludedObjectNames<SpeciesDto>(lstOfFields);
+                }
+
+                var item = _service.GetItemById(id);
+
+                if (item == null)
+                {
+                    return NotFound();
+                }
+
+                var itemDto = Mapper.Map<JournalEntry, JournalEntryDto>(item);
+
+                return Ok(DataShaping.CreateDataShapedObject(itemDto, lstOfFields));
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+
+            // if bad request, return 400
+            //return BadRequest();
+        }
+
+        // POST: api/Plant
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]CreateUpdateJournalEntryDto dtoIn)
+        {
+            // TODO: Add validation checks (e.g. uniqueness)
+            try
+            {
+                if (dtoIn == null)
+                {
+                    return BadRequest();
+                }
+
+                var entity = Mapper.Map<CreateUpdateJournalEntryDto, JournalEntry>(dtoIn);
+
+                var returnEntity =_service.Add(entity);
+
+                // Save changes before we map back
+                var changes = _unitOfWorkAsync.SaveChanges();
+
+                // Check for errors from service
+                if (changes > 0)
+                {
+                    var dtoOut = Mapper.Map<JournalEntry, JournalEntryDto>(entity);
+
+                    var location = Request.RequestUri + "/" + dtoOut.Id;
+                    return Created(location, dtoOut);
+                }
+
+                return BadRequest();
+            }
+            catch (Exception e)
+            {
+                return InternalServerError();
+            }
+        }
+
+        // PUT: api/Plant/5
+        // TODO: Make underlying operation FULL update only (i.e. all stored fields, or default values if not supplied)
+        [HttpPut]
+        public IHttpActionResult Put(int id, [FromBody]CreateUpdateJournalEntryDto dtoIn)
+        {
+            try
+            {
+                // Handle mapping failure - where dto is not in right format
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                if (dtoIn == null) 
+                {
+                    return BadRequest();
+                }
+
+                // Find id without tracking to prevent attaching object (and hence problem when attaching via save)
+                var entityFound = _service.Queryable().AsNoTracking().FirstOrDefault(g => g.Id == id);
+                if (entityFound == null)
+                {
+                    return NotFound();
+                }
+
+                var entity = Mapper.Map<CreateUpdateJournalEntryDto, JournalEntry>(dtoIn);
+                entity.Id = entityFound.Id;
+
+                var returnEntity = _service.Save(entity);
+
+                // Save changes before we map back
+                var changes = _unitOfWorkAsync.SaveChanges();
+
+                // Check for errors from service
+                if (changes > 0)
+                {
+                    var dtoOut = Mapper.Map<JournalEntry, JournalEntryDto>(entity);
+
+                    return Ok(dtoOut);
+                }
+
+                return BadRequest();
+            }
+            catch (Exception e)
+            {
+                return InternalServerError();
+            }
+        }
+
+        // PATCH: api/Plant/5
+        // Partial update
+        [HttpPatch]
+        public IHttpActionResult Patch(int id, [FromBody]JsonPatchDocument<CreateUpdateJournalEntryDto> itemPatchDoc)
+        {
+            try
+            {
+                if (itemPatchDoc == null)
+                {
+                    return BadRequest();
+                }
+
+                // Get domain entity
+                // Find id without tracking to prevent attaching object (and hence problem when attaching via save)
+                var entityFound = _service.Queryable().AsNoTracking().FirstOrDefault(g => g.Id == id);
+                // Check for errors from service
+                if (entityFound == null)
+                {
+                    return NotFound();
+                }
+
+                // Map to dto
+                var dtoFound = Mapper.Map<JournalEntry, CreateUpdateJournalEntryDto>(entityFound);
+
+                // Apply changes to dto
+                itemPatchDoc.ApplyTo(dtoFound);
+
+                var updatedEntity = Mapper.Map<CreateUpdateJournalEntryDto, JournalEntry>(dtoFound);
+                updatedEntity.Id = id;
+
+                var returnEntity = _service.Save(updatedEntity);
+
+                // Save changes before we map back
+                var changes = _unitOfWorkAsync.SaveChanges();
+
+                // Check for errors from service
+                if (changes > 0)
+                {
+                    var dtoOut = Mapper.Map<JournalEntry, JournalEntryDto>(updatedEntity);
+
+                    return Ok(dtoOut);
+                }
+
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+        // DELETE: api/Plant/5
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                // Get domain entity
+                var entityFound = _service.GetItemById(id);
+
+                // Check for errors from service
+                if (entityFound == null)
+                {
+                    return NotFound();
+                }
+
+                _service.Delete(entityFound);
+                _unitOfWorkAsync.SaveChanges();
+                
+                // return 204 (also via void return type)
+                return StatusCode(HttpStatusCode.NoContent);
+                
+                
+                //return BadRequest();
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
