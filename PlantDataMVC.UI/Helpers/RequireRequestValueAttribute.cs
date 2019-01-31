5cb531fc2d713222707f89da04bb27036062e72c
Timmo <mcmaster.tim@gmail.com>
2019-01-31 23:13:02 +1000

    Comments on RequireRequestValueAttribute

--- PlantDataMVC.UI/Helpers/RequireRequestValueAttribute.cs
+++ PlantDataMVC.UI/Helpers/RequireRequestValueAttribute.cs
@@ -3,16 +3,22 @@ using System.Web.Mvc;
 
 namespace PlantDataMVC.UI.Helpers
 {
+    /// <summary>
+    /// Implements method overloading via attribute based on parameter name in request
+    /// Called as <code>[RequireRequestValue("speciesId")]</code>
+    /// </summary>
     public class RequireRequestValueAttribute : ActionMethodSelectorAttribute
     {
         public RequireRequestValueAttribute(string valueName)
         {
             ValueName = valueName;
         }
+
         public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
         {
             return controllerContext.HttpContext.Request[ValueName] != null;
         }
+
         public string ValueName { get; }
     }
 }
