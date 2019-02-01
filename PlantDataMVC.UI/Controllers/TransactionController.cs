581a960c56504fd3ae3e70eb32771dce6551d865
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2019-02-01 17:16:45 +1000

    Minor changes to transaction controllers and views

--- PlantDataMVC.UI/Controllers/TransactionController.cs
+++ PlantDataMVC.UI/Controllers/TransactionController.cs
@@ -28,7 +28,7 @@ namespace PlantDataMVC.UI.Controllers
             _httpClient = httpClient;
         }
 
-/*
+        /*
         // GET: /"ControllerName"/Index
         // GET: /"ControllerName"/Index?page=4&pageSize=20&sortBy=Genus&ascending=True
         public override async Task<ActionResult> Index(int? page, int? pageSize, string sortBy, bool? ascending)
@@ -48,21 +48,29 @@ namespace PlantDataMVC.UI.Controllers
 
             return ListView<PlantStockTransactionListViewModel>(autoMapResult, localPage, localPageSize, localSortBy, localAscending);
         }
+        */
 
         //
         // GET: /"ControllerName"/Show/5
-        public async override Task<ActionResult> Show(int id)
+        public async Task<ActionResult> Show(int id)
         {
-            // return view for Model
-            IViewResponse<JournalEntryDto> response = _dataService.View(id);
+            var httpResponse = await _httpClient.GetAsync("api/JournalEntries/" + id);
 
-            JournalEntryDto item = response.Item;
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                string content = await httpResponse.Content.ReadAsStringAsync();
+                var model = JsonConvert.DeserializeObject<JournalEntryDto>(content);
 
-            // TODO: check to ensure these DTOs map to view model
-            return AutoMapView<PlantStockTransactionShowViewModel>(View(item));
+                // TODO: check to ensure these DTOs map to view model
+                return AutoMapView<PlantStockTransactionShowViewModel>(View(model));
+            }
+            else
+            {
+                return Content("An error occurred");
+            }
         }
-*/
-/*
+        
+        /*
         //
         // GET: /"ControllerName"/New
         public override ActionResult New()
@@ -70,7 +78,8 @@ namespace PlantDataMVC.UI.Controllers
             var item = new PlantStockTransactionNewViewModel();
             return View(item);
         }
-*/
+        */
+
         /// <summary>
         /// Additional action for creating a new entry for a given plant stock entry.
         /// </summary>
