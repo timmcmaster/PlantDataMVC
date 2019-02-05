f8d13a70d2b7b592dcb568e9f3384a2c7d321209
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2019-02-05 17:40:10 +1000

    WIP: Getting dropdowns to work without hanging.

--- PlantDataMVC.UI/Helpers/DropdownExtensions.cs
+++ PlantDataMVC.UI/Helpers/DropdownExtensions.cs
@@ -3,24 +3,81 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Web.ModelBinding;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
-using PlantDataMVC.DTO.Dtos;
+using Framework.Web.Mediator;
+using Framework.Web.Views;
+using PlantDataMVC.UI.Models.ViewModels;
 
 namespace PlantDataMVC.UI.Helpers
 {
-    /// <summary>
-    /// Usage @Html.DropDownFor(() => "ProductType.Id", m => m.ProductType, p => p.DisplayValue, p => p.Id)
-    /// </summary>
     public static class DropDownExtensions
     {
-        public static MvcHtmlString DropDownFor<TModel, TDtoItem>(this HtmlHelper<TModel> htmlHelper,
-            Func<string> fieldName,
-            Expression<Func<TModel, TDtoItem>> expression,     // Selects the referenced entity from the model
-            Func<TDtoItem, string> displayValueSelector,       // Selects the display field from the entity
-            Func<TDtoItem, object> dataValueSelector           // Selects the value field from the entity
-            ) where TDtoItem : class, IDto
+        ///// <summary>
+        ///// Usage @Html.DropDownFor(() => "ProductType.Id", m => m.ProductType, p => p.DisplayValue, p => p.Id)
+        ///// </summary>
+        //public static MvcHtmlString DropDownFor<TModel, TDtoItem>(this HtmlHelper<TModel> htmlHelper,
+        //    Func<string> fieldName,
+        //    Expression<Func<TModel, TDtoItem>> expression,     // Selects the referenced entity from the model
+        //    Func<TDtoItem, string> displayValueSelector,       // Selects the display field from the entity
+        //    Func<TDtoItem, object> dataValueSelector           // Selects the value field from the entity
+        //    ) where TDtoItem : class, IDto
+        //{
+        //    var expressionText = ExpressionHelper.GetExpressionText(expression);
+
+        //    ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+
+        //    var selectedItem = (TDtoItem)metadata.Model;
+
+        //    // Get list of options from dataService
+
+        //    // TODO: fix to get corresponding hierarchy in a better way
+        //    // This now works after implementing ServiceContract and OperationContract on IDataServiceBase<T>
+        //    // Can't get base service directly, as it's not registered with Autofac
+        //    //Type interfaceType = GetDataServiceInterfaceFor<TItem>();
+        //    //var dataService = DependencyResolver.Current.GetService(interfaceType) as IDataServiceBase<TItem>;
+
+        //    IWcfService dataService = GetServiceForDto<TDtoItem>();
+
+        //    IList<TDtoItem> items = new List<TDtoItem>();
+        //    if (dataService != null)
+        //    {
+        //        items = dataService.List<TDtoItem>().Items;
+        //    }
+
+        //    IEnumerable<SelectListItem> selectListItems = items.Select(x => new SelectListItem
+        //    {
+        //        Text = displayValueSelector(x),
+        //        Value = dataValueSelector(x).ToString(),
+        //        Selected = dataValueSelector(x).Equals(dataValueSelector(selectedItem))
+        //    });
+
+        //    return htmlHelper.DropDownList(fieldName(), selectListItems, "Select an option");
+        //}
+
+        /// <summary>
+        /// Query drop down for the model.
+        /// Usage @Html.DropDownFor(() => "ProductType.Id", new ProductListQuery(),m => m.ProductType, p => p.DisplayValue, p => p.Id)
+        /// </summary>
+        /// <typeparam name="TModel">The type of the viewmodel for the view.</typeparam>
+        /// <typeparam name="TDtoItem">The type of the dto item.</typeparam>
+        /// <typeparam name="TQuery">The type of the query.</typeparam>
+        /// <param name="htmlHelper">The HTML helper for the page viewmodel (TModel).</param>
+        /// <param name="fieldName">Name of the field.</param>
+        /// <param name="expression">Selects the referenced item related to this dropdown entity from the model.</param>
+        /// <param name="query">The query that returns an enumerable of the dropdown entity type.</param>
+        /// <param name="displayValueSelector">Selects the display field from the dropdown entity.</param>
+        /// <param name="dataValueSelector">Selects the value field from the dropdown entity</param>
+        /// <returns></returns>
+        public static MvcHtmlString QueryDropDownFor<TModel, TDtoItem, TQuery>(this HtmlHelper<TModel> htmlHelper,
+                                                                               Func<string> fieldName,
+                                                                               //Expression<Func<TModel, TDtoItem>> expression,
+                                                                               Expression<Func<TModel, TDtoItem>> expression,
+                                                                               TQuery query,
+                                                                               Func<TDtoItem, string> displayValueSelector,
+                                                                               Func<TDtoItem, object> dataValueSelector
+        ) where TDtoItem : class, IDto
+          where TQuery : IViewQuery<IEnumerable<TDtoItem>>
         {
             var expressionText = ExpressionHelper.GetExpressionText(expression);
 
@@ -28,23 +85,13 @@ namespace PlantDataMVC.UI.Helpers
 
             var selectedItem = (TDtoItem)metadata.Model;
 
-            // Get list of options from dataService
+            // Get list of options via query
+            var mediator = DependencyResolver.Current.GetService<IMediator>();
+            var requestTask = mediator.Request(query);
+            requestTask.Wait();
+            var dtoItems = requestTask.Result;
 
-            // TODO: fix to get corresponding hierarchy in a better way
-            // This now works after implementing ServiceContract and OperationContract on IDataServiceBase<T>
-            // Can't get base service directly, as it's not registered with Autofac
-            //Type interfaceType = GetDataServiceInterfaceFor<TItem>();
-            //var dataService = DependencyResolver.Current.GetService(interfaceType) as IDataServiceBase<TItem>;
-
-            IWcfService dataService = GetServiceForDto<TDtoItem>();
-
-            IList<TDtoItem> items = new List<TDtoItem>();
-            if (dataService != null)
-            {
-                items = dataService.List<TDtoItem>().Items;
-            }
-
-            IEnumerable<SelectListItem> selectListItems = items.Select(x => new SelectListItem
+            IEnumerable<SelectListItem> selectListItems = dtoItems.Select(x => new SelectListItem
             {
                 Text = displayValueSelector(x),
                 Value = dataValueSelector(x).ToString(),
@@ -54,39 +101,45 @@ namespace PlantDataMVC.UI.Helpers
             return htmlHelper.DropDownList(fieldName(), selectListItems, "Select an option");
         }
 
-        // HACK: Still very hacky quick method to return service interface for given IDto type
-        public static IWcfService GetServiceForDto<TDtoItem>() where TDtoItem : IDto
+        /// <summary>
+        /// Query drop down for the model.
+        /// Usage @Html.DropDownFor(() => "ProductType.Id", new ProductListQuery(),m => m.ProductType, p => p.DisplayValue, p => p.Id)
+        /// </summary>
+        /// <typeparam name="TModel">The type of the viewmodel for the view.</typeparam>
+        /// <typeparam name="TListItem">The type of the dto item.</typeparam>
+        /// <typeparam name="TQuery">The type of the query.</typeparam>
+        /// <param name="htmlHelper">The HTML helper for the page viewmodel (TModel).</param>
+        /// <param name="fieldName">Name of the form field to be used.</param>
+        /// <param name="expression">Selects the referenced item related to this dropdown entity from the model.</param>
+        /// <param name="query">The query that returns an enumerable of the dropdown entity type.</param>
+        /// <param name="displayValueSelector">Selects the display field from the dropdown entity.</param>
+        /// <param name="dataValueSelector">Selects the value field from the dropdown entity</param>
+        /// <returns></returns>
+        public static MvcHtmlString QueryDropDown2For<TModel, TListItem, TQuery>(this HtmlHelper<TModel> htmlHelper,
+                                                                                 TListItem sampleItem,
+                                                                               Func<string> fieldName,
+                                                                               Expression<Func<TModel, object>> selectedDataValue,
+                                                                               TQuery query,
+                                                                               Func<TListItem, string> displayValueSelector,
+                                                                               Func<TListItem, object> dataValueSelector
+        ) where TListItem : class
+          where TQuery : IViewQuery<ListViewModelStatic<TListItem>>
         {
-            // TODO: Add all DTO types used by dropdown in here
-            if (typeof(TDtoItem) == typeof(GenusDto))
-                return GetServiceFor<IGenusWcfService>();
-            //else if (typeof(TItem) == typeof(PlantPriceListType))
-            //    return typeof();
-            //else if (typeof(TItem) == typeof(PlantProductPrice))
-            //    return typeof();
-            else if (typeof(TDtoItem) == typeof(SpeciesDto))
-                return GetServiceFor<ISpeciesWcfService>();
-            else if (typeof(TDtoItem) == typeof(ProductTypeDto))
-                return GetServiceFor<IProductTypeWcfService>();
-            else if (typeof(TDtoItem) == typeof(SeedBatchDto))
-                return GetServiceFor<ISeedBatchWcfService>();
-            else if (typeof(TDtoItem) == typeof(SiteDto))
-                return GetServiceFor<ISiteWcfService>();
-            else if (typeof(TDtoItem) == typeof(SeedTrayDto))
-                return GetServiceFor<ISeedTrayWcfService>();
-            else if (typeof(TDtoItem) == typeof(PlantStockDto))
-                return GetServiceFor<IPlantStockWcfService>();
-            else if (typeof(TDtoItem) == typeof(JournalEntryDto))
-                return GetServiceFor<IJournalEntryWcfService>();
-            else if (typeof(TDtoItem) == typeof(JournalEntryTypeDto))
-                return GetServiceFor<IJournalEntryTypeWcfService>();
-            else
-                throw new Exception("corresponding service not found");
+            // Get list of options via query
+            var mediator = DependencyResolver.Current.GetService<IMediator>();
+            var requestTask = mediator.Request(query);
+            requestTask.Wait();
+            var dtoItems = requestTask.Result;
+
+            IEnumerable<SelectListItem> selectListItems = dtoItems.Select(x => new SelectListItem
+            {
+                Text = displayValueSelector(x),
+                Value = dataValueSelector(x).ToString(),
+                Selected = dataValueSelector(x).Equals(selectedDataValue)
+            });
+
+            return htmlHelper.DropDownList(fieldName(), selectListItems, "Select an option");
         }
 
-        public static IWcfService GetServiceFor<TInterface>()
-        {
-            return DependencyResolver.Current.GetService<TInterface>() as IWcfService;
-        }
     }
 }
