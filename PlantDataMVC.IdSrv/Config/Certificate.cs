5abd63f913b2bad4f2afec6e42a751b2ea820a46
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2019-02-12 13:41:08 +1000

    Got basic OAuth2/OpenID server setup

---
+++ PlantDataMVC.IdSrv/Config/Certificate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using IdentityServer3.Core.Models;
+using PlantDataMVC.Constants;
+
+namespace PlantDataMVC.IdSrv.Config
+{
+    public static class Certificate
+    {
+        public static X509Certificate2 Get()
+        {
+            // Get this assembly
+            var assembly = typeof(Certificate).Assembly;
+
+            var names = assembly.GetManifestResourceNames();
+
+            using (var stream = assembly.GetManifestResourceStream("PlantDataMVC.IdSrv.Certificates.idsrv3test.pfx"))
+            {
+                return new X509Certificate2(ReadStream(stream), "idsrv3test");
+            }
+        }
+
+        private static byte[] ReadStream(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+}
--- PlantDataMVC.IdSrv/Startup.cs
+++ PlantDataMVC.IdSrv/Startup.cs
@@ -16,7 +16,6 @@ namespace PlantDataMVC.IdSrv
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
-
             app.Map("/identity", idsrvApp =>
             {
                 idsrvApp.UseIdentityServer(new IdentityServerOptions
@@ -29,17 +28,9 @@ namespace PlantDataMVC.IdSrv
                               .UseInMemoryUsers(Users.Get())
                               .UseInMemoryScopes(Scopes.Get()),
 
-                    SigningCertificate = LoadCertificate() 
+                    SigningCertificate = Certificate.Get() 
                 });
             });
         }
-
-        X509Certificate2 LoadCertificate()
-        {
-            return new X509Certificate2(
-                string.Format(@"{0}\bin\idsrv3test.pfx",
-                              AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
-        }
-
     }
 }
