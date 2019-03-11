be8758abb3f13c330f30f520cf761f728e007e98
RACQGROUP\U007696 <tim.mcmaster@racq.com.au>
2019-03-11 16:59:06 +1000

    Added view for Plant

---
+++ PlantDataDB/dbo/Views/Plant.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [dbo].[Plant]
+	AS SELECT S.Id, G.LatinName, S.SpecificName, S.CommonName, S.Description, S.PropagationTime, S.Native FROM dbo.Species S
+	left join dbo.Genus G
+	ON S.GenusId = G.Id
