d7da0acf51cfc354eeb00c780fdd280bb390ab2c
timmcmaster <mcmaster.tim@gmail.com>
2022-05-02 22:54:57 +1000

    WIP: Created UICore project

---
+++ PlantDataMvc.UICore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlantDataMvc.UICore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PlantDataMvc.UICore/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.AspNetCore.Mvc.Versioning;
+
+namespace PlantDataMVC.UICore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            // In ASP.NET Core 3.x, using `Host.CreateDefaultBuilder` (as in the preceding Program.cs snippet) will
+            // set up some configuration for you based on your appsettings.json and environment variables. See "Remarks" at
+            // https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.hosting.host.createdefaultbuilder for details.
+            this.Configuration = configuration;
+        }
+
+        // ConfigureServices is where you register dependencies.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+
+        // Configure is where you add middleware.        
+        // You can use IApplicationBuilder.ApplicationServices
+        // here if you need to resolve things from the container.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment host)
+        {
+            if (!host.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
