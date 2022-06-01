cad6d1409897d42158710580e8f6f3fca0d66656
Tim McMaster <tim.mcmaster@bradnams.com.au>
2022-06-02 08:06:15 +1000

    Added SaleEvent table to DB

---
+++ PlantDataDB/dbo/Tables/SaleEvent.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[SaleEvent] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (30) NOT NULL,
+    [SaleDate] DATE NULL,
+    [Location] NVARCHAR (30) NULL,
+    CONSTRAINT [PK_SaleEvent] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
