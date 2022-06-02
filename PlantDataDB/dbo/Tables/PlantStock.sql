996a83acf2ec9d94bd9ab36446e945415ee0004d
Tim McMaster <tim.mcmaster@bradnams.com.au>
2022-06-02 13:38:54 +1000

    Added SaleEventStock table

--- PlantDataDB/dbo/Tables/PlantStock.sql
+++ PlantDataDB/dbo/Tables/PlantStock.sql
@@ -3,7 +3,7 @@
     [SpeciesId]       INT NOT NULL,
     [ProductTypeId]   INT NOT NULL,
     [QuantityInStock] INT NOT NULL,
-    CONSTRAINT [PK_PlantStock_1] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [PK_PlantStock] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_PlantStock_ProductType] FOREIGN KEY ([ProductTypeId]) REFERENCES [dbo].[ProductType] ([Id]),
     CONSTRAINT [FK_PlantStock_Species] FOREIGN KEY ([SpeciesId]) REFERENCES [dbo].[Species] ([Id]),
     CONSTRAINT [UK_PlantStock] UNIQUE NONCLUSTERED ([SpeciesId] ASC, [ProductTypeId] ASC)
---
+++ PlantDataDB/dbo/Tables/SaleEventStock.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SaleEventStock]
+(
+	[Id] INT IDENTITY (1,1) NOT NULL,
+	[SaleEventId] INT NOT NULL,
+	[PlantStockId] INT NOT NULL,
+	[Quantity] INT NOT NULL,
+    CONSTRAINT [PK_SaleEventStock] PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_SaleEventStock_SaleEvent] FOREIGN KEY ([SaleEventId]) REFERENCES [SaleEvent]([Id]),
+    CONSTRAINT [FK_SaleEventStock_PlantStock] FOREIGN KEY ([PlantStockId]) REFERENCES [PlantStock]([Id])
+);
