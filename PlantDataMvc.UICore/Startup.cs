dc93bcf974fe3d15125e53e1b02a31adba53e7fc
Tim McMaster <tim.mcmaster@bradnams.com.au>
2022-06-02 15:15:08 +1000

    Simplified default controller route

--- PlantDataMvc.UICore/Startup.cs
+++ PlantDataMvc.UICore/Startup.cs
@@ -178,10 +178,7 @@ namespace PlantDataMVC.UICore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}")
-                .RequireAuthorization();
+                endpoints.MapDefaultControllerRoute().RequireAuthorization();
             });
         }
     }
