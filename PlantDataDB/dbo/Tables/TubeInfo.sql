941e2279e4fba7d0db000374edde1c6cd5b88515
timmcmaster <mcmaster.tim@gmail.com>
2023-02-16 22:37:35 +1000

    DB cleanup

--- PlantDataDB/dbo/Tables/TubeInfo.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[TubeInfo] (
-    [Id]             INT  IDENTITY (1, 1) NOT NULL,
-    [SeedTrayId]     INT  NOT NULL,
-    [DatePrickedOut] DATE NULL,
-    [NumberOfTubes]  INT  NULL,
-    CONSTRAINT [PK_TubeInfo] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_TubeInfo_SeedPlanting] FOREIGN KEY ([SeedTrayId]) REFERENCES [dbo].[SeedTray] ([Id])
-);
-
