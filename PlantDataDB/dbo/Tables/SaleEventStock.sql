6a03ed3d22d3c46e0fee2087ed93ad3c94d0d669
timmcmaster <mcmaster.tim@gmail.com>
2023-03-05 01:17:55 +1000

    DB project changes

--- PlantDataDB/dbo/Tables/SaleEventStock.sql
+++ PlantDataDB/dbo/Tables/SaleEventStock.sql
@@ -2,9 +2,11 @@
 (
 	[Id] INT IDENTITY (1,1) NOT NULL,
 	[SaleEventId] INT NOT NULL,
-	[PlantStockId] INT NOT NULL,
+    [SpeciesId]          INT           NOT NULL,
+    [ProductTypeId]      INT           NOT NULL,
 	[Quantity] INT NOT NULL,
     CONSTRAINT [PK_SaleEventStock] PRIMARY KEY CLUSTERED ([Id] ASC), 
     CONSTRAINT [FK_SaleEventStock_SaleEvent] FOREIGN KEY ([SaleEventId]) REFERENCES [SaleEvent]([Id]),
-    CONSTRAINT [FK_SaleEventStock_PlantStock] FOREIGN KEY ([PlantStockId]) REFERENCES [PlantStock]([Id])
+    CONSTRAINT [FK_SaleEventStock_Species] FOREIGN KEY ([SpeciesId]) REFERENCES [Species]([Id]),
+    CONSTRAINT [FK_SaleEventStock_ProductType] FOREIGN KEY ([ProductTypeId]) REFERENCES [ProductType]([Id])
 );
--- PlantDataDB/dbo/Tables/SeedBatch.sql
+++ PlantDataDB/dbo/Tables/SeedBatch.sql
@@ -5,6 +5,7 @@
     [Location]      NVARCHAR (30)  NULL,
     [Notes]         NVARCHAR (200) NULL,
     [SiteId]        INT            NULL,
+    [Emptied] BIT NOT NULL , 
     CONSTRAINT [PK_SeedBatch] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_SeedBatch_Site] FOREIGN KEY ([SiteId]) REFERENCES [dbo].[Site] ([Id]),
     CONSTRAINT [FK_SeedBatch_Species] FOREIGN KEY ([SpeciesId]) REFERENCES [dbo].[Species] ([Id])
