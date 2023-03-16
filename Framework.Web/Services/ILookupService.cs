df2225c9eb8a9280db661d56658bbbef6e4eeffe
timmcmaster <mcmaster.tim@gmail.com>
2023-03-16 21:54:07 +1000

    Lookup stuff

---
+++ Framework.Web/Services/ILookupService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Framework.Web.Services
+{
+    public interface ILookupService<TItem> where TItem : class
+    {
+        IEnumerable<TItem> GetData();
+        IEnumerable<TItem> GetOrderedData(Func<TItem, string> displayValueSelector);
+    }
+}
