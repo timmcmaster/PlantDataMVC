50eced4a943bdb3e40818eb61fa3308d6b9b694e
Tim McMaster <tim.mcmaster@bradnams.com.au>
2023-05-16 13:18:26 +1000

    Package updates

--- PlantDataMVC.Web/Startup.cs
+++ PlantDataMVC.Web/Startup.cs
@@ -165,7 +165,7 @@ namespace PlantDataMVC.Web
         // here if you need to resolve things from the container.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt+QHFqVk9rXVNbdV5dVGpAd0N3RGlcdlR1fUUmHVdTRHRcQlliT35VckJiW3ZZcX0=;Mgo+DSMBPh8sVXJ1S0d+X1hPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSXpScEVlXXhfcn1dQmk=;ORg4AjUWIQA/Gnt2VFhhQlJNfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5Xd0NjX3tZcXZcT2hV;MTczODQxN0AzMjMxMmUzMTJlMzMzOWtzWUpjU2NkbFJyUXU2bTU2bW5EYmt5bisxaVB6SkJNOE5hVVhDQ1BYRE09;MTczODQxOEAzMjMxMmUzMTJlMzMzOUY2STA3UmxrQktqekc5WXN4Y3lOeWIvTXBnZDZCVWgrdDVMWDRHQnZkc2M9;NRAiBiAaIQQuGjN/V0d+XU9Hf1RDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS31TckRlWXlad3VWRmZUWA==;MTczODQyMEAzMjMxMmUzMTJlMzMzOWpGZUZuZEo2K1Q5SDI3NXNGUjJQTERvRU41Z1JKV296V01CcjR1SnNxVE09;MTczODQyMUAzMjMxMmUzMTJlMzMzOWJmaWpZOG5veVV5aHBhL0ZVZkpmQzR0R3g4cXhoYnZrVTVJajlINHRhd289;Mgo+DSMBMAY9C3t2VFhhQlJNfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5Xd0NjX3tZcXdUQmBV;MTczODQyM0AzMjMxMmUzMTJlMzMzOUZoZ1RMcnMyazVWVkhPcUdyQkdIbzIxZUZ4dW0rZ1pSRGFhSnhtTEppVHM9;MTczODQyNEAzMjMxMmUzMTJlMzMzOWtaUjd2TFpXRUtBYlVQUTIvUER0eDN3LzZMekI0Rm1xL0tRN1BQQytQZVU9;MTczODQyNUAzMjMxMmUzMTJlMzMzOWpGZUZuZEo2K1Q5SDI3NXNGUjJQTERvRU41Z1JKV296V01CcjR1SnNxVE09");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VFhiQlJPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSXtSc0diXHpeeXBVRWc=");
 
             // Stop trying to map tokens to .Net claim types
             JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
