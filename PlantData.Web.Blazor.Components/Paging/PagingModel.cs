9501520d0b374cdc609f0cb6390c9dfd797a51ec
Tim McMaster <mcmaster.tim@gmail.com>
2024-11-13 21:52:48 +1000

    WIP: paging

--- PlantData.Web.Blazor.Components/Paging/PagingModel.cs
+++ PlantData.Web.Blazor.Components/Paging/PagingModel.cs
@@ -1,4 +1,6 @@
-﻿namespace PlantData.Web.Blazor.SharedComponents.Paging
+﻿using System;
+
+namespace PlantData.Web.Blazor.SharedComponents.Paging
 {
     public class PagingModel
     {
@@ -8,5 +10,19 @@
         public bool HasPreviousPage { get; set; }
         public int TotalCount { get; set; }
         public int TotalPages { get; set; }
+
+        public PagingModel(int pageSize, int pageNumber, int totalCount)
+        {
+            var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            var hasPrevious = pageNumber > 1;
+            var hasNext = pageNumber < totalPages;
+
+            PageSize = pageSize;
+            PageNumber = pageNumber;
+            TotalCount = totalCount;
+            TotalPages = totalPages;
+            HasPreviousPage = hasPrevious;
+            HasNextPage = hasNext;
+        }
     }
 }
