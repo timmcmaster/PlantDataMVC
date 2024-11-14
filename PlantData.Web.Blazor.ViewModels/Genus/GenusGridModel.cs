d4d1c5b2c004e81ec3de84e1522263d203c33c5c
Tim McMaster <mcmaster.tim@gmail.com>
2024-11-14 13:04:26 +1000

    WIP: Fix to grid cell display

---
+++ PlantData.Web.Blazor.ViewModels/Genus/GenusGridModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace PlantData.Web.Blazor.ViewModels.Genus
+{
+    public class GenusGridModel
+    {
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Latin Name")]
+        public string LatinName { get; set; }
+    }
+}
