9e3d6cc5d267d022694c883f2c48a2839bfdc4fc
Tim McMaster <mcmaster.tim@gmail.com>
2024-12-18 13:19:58 +1000

    Identity Server set namespace

--- PlantDataMVC.IdentityServer/Program.cs
+++ PlantDataMVC.IdentityServer/Program.cs
@@ -3,40 +3,50 @@ using PlantDataMVC.IdentityServer;
 using Serilog;
 using System;
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.File("..\\logs\\IdentityServer-bootstrap-log-.txt",
-        rollingInterval: RollingInterval.Day)
-    .CreateBootstrapLogger();
-
-Log.Information("Starting up");
-
-try
+namespace PlantDataMVC.IdentityServer
 {
-    var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
 
-    builder.Host.UseSerilog((ctx, lc) => lc
-        .ReadFrom.Configuration(ctx.Configuration)
-        .Enrich.FromLogContext()
-        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
-        .WriteTo.File("..\\logs\\IdentityServer-log-.txt",
-            outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext} {Message:lj} {Exception}{NewLine}",
-            rollingInterval: RollingInterval.Day)
-        .MinimumLevel.Debug()
-    );
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("..\\logs\\IdentityServer-bootstrap-log-.txt",
+                rollingInterval: RollingInterval.Day)
+            .CreateBootstrapLogger();
 
-    var app = builder
-        .ConfigureServices()
-        .ConfigurePipeline();
+            Log.Information("Starting up");
 
-    app.Run();
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Unhandled exception");
-}
-finally
-{
-    Log.Information("Shut down complete");
-    Log.CloseAndFlush();
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog((ctx, lc) => lc
+                    .ReadFrom.Configuration(ctx.Configuration)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                    .WriteTo.File("..\\logs\\IdentityServer-log-.txt",
+                        outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext} {Message:lj} {Exception}{NewLine}",
+                        rollingInterval: RollingInterval.Day)
+                    .MinimumLevel.Debug()
+                );
+
+                var app = builder
+                    .ConfigureServices()
+                    .ConfigurePipeline();
+
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Unhandled exception");
+            }
+            finally
+            {
+                Log.Information("Shut down complete");
+                Log.CloseAndFlush();
+            }
+        }
+    }
 }
