d15da13d2d99497adcd91149d2c081adea04663c
Tim McMaster <mcmaster.tim@gmail.com>
2026-05-15 21:19:49 +1000

    PDF file opening

---
+++ PlantData.Web.Blazor/wwwroot/js/fileHelpers.js
@@ -0,0 +1,11 @@
+function openPdfInNewTab(base64Data, fileName) {
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: 'application/pdf' });
+    const url = URL.createObjectURL(blob);
+    window.open(url, '_blank');
+}
